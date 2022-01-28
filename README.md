70ab7bc64e229cb5871a65713d81a0714b84f987
Changhyun Kim <movementfor@gmail.com>
2022-01-28 17:45:03 +0000

    Updated logs in README file

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ This tutorial is based on the video from freecodecamp.org\
 ### 2022-01-27
 - Learned about Hooks (e.g `useState`, `useEffect`) to manage the states in React
 - Loaded some given images and applied styles
+
+### 2022-01-28
+- Built custom hook and understood how it works
+- Applied styles to each component
+- Grid style to display movie thumbnails
+- Rendered Spinner
+- Implemented search bar
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,51 @@
-import React, { useState, useEffect } from 'react';
-// API
-import API from '../API';
+import React from 'react';
 
 // config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
-
+import { useHomeFetch } from '../Hooks/useHomeFetch';
 // Images
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchTerm = '') => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-
-            setState(prev => ({
-                ...movies, 
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-
-        }catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    };
-
-    // Initial render
-    useEffect(() => {
-        fetchMovies(1);
-    }, []);
-
-    console.log(state);
     
-    return <div>Home page</div>
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
+
+    
+    return (
+        <>
+            {state.results[0] ? (
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            /> 
+            ) : null}
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+            <Spinner />
+        </>
+    );
 }
 
 export default Home;
